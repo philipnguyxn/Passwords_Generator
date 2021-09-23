1fb4bc0ee5f2af8c171c5f02ba4a4506f99d86ba
philipnguyxn <khoinm2003@gmail.com>
2021-09-23 15:42:32 +0700

    Final

--- main.cpp
+++ main.cpp
@@ -1,69 +1,35 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <cstdlib>
 #include <iostream>
-#include <string>
+#include <ctime>
 
 using namespace std;
 
-string Generator( string* alphabets_numbers, int upperLimit ){
-    string passwords;
-    srand(time(0));
+static const char alphabets_numbers [] = {"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*/-"} ;
+int length = sizeof(alphabets_numbers) - 1;
 
-    for ( int i = 0 ; i < upperLimit; i++ )
+string Generator( int size ){
+    string passwords;
+
+    for ( int i = 0 ; i < size; i++ )
     {
-        passwords += alphabets_numbers[ (rand() % upperLimit) - 1 ];
+        int index = rand() % length;
+        passwords += alphabets_numbers[index];
     }
     return passwords;
 }
 
 int main() {
-    int upperLimit;
-    cout << "Enter the upper limit you want: ";
-    cin >> upperLimit;
+    srand(time(0));
 
-    bool upper_lower_required;
-    cout << "Do your password need to be in uppercase: ";
-    cin >> upper_lower_required;
+    int size;
+    cout << "Enter the password size you want: ";
+    cin >> size;
 
-    string special_characters;
-    cout << "Enter what special characters you  want: ";
-    cin >> special_characters;
-
-    vector <string> alphabets_numbers = {
-        "0123456789",
-        "abcdefghijklmnopqrstuvwxyz",
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    };
-
-    if ( upper_lower_required == true &&  special_characters == " " )
-    {
-        alphabets_numbers.push_back( "`~!@#$%^&*/-" );
-    }
-    else if ( upper_lower_required == true &&  special_characters != " " )
-    {
-        for ( int i = 0; i < special_characters.length(); i++)
-        {
-            alphabets_numbers.push_back( special_characters );
-        }
-    }
-    else if ( upper_lower_required != true &&  special_characters == " " )
-    {
-        alphabets_numbers.push_back( "`~!@#$%^&*/-" );
-        alphabets_numbers.pob_back( "ABCDEFGHIJKLMNOPQRSTUVWXYZ" );
-    }
-    else
-    {
-        alphabets_numbers.pob_back( "ABCDEFGHIJKLMNOPQRSTUVWXYZ" );
-        for ( int i = 0; i < special_characters.length() ; i++)
-        {
-            alphabets_numbers.push_back( special_characters );
-        }
-    }
-    string* p_alphabet_numbers = &alphabets_numbers[0];
-
-    string passwords = Generator( p_alphabet_numbers, upperLimit );
+    string passwords = Generator(size);
     cout << "Here's your passwords: " << passwords << endl;
 
     return 0;
 }
+
+// g++ -std=c++17 main.cpp
+// ./a.out
